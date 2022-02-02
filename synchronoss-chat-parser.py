d920573e2065d13952ebfbc9b7775b1059078dd6
Fl4sh Designz <87091821+Fl4shDesignz@users.noreply.github.com>
2022-02-01 21:03:47 -0500

    Working Sample
    Working example of single file as arg and successfully loading, reading, converting and saving out as JSON file in new dir. Built in logic to test if directory and file already exists.

--- synchronoss-chat-parser.py
+++ synchronoss-chat-parser.py
@@ -6,6 +6,7 @@ import re
 # import tkinter as tk
 # from tkinter.filedialog import askopenfilename
 import os
+from tabnanny import check
 import time
 import csv
 import sys
@@ -81,6 +82,85 @@ def check_args(arg):
         return arg, '', errors
 
 
+def get_JSON_File(user_arg, full_file_name):
+    split_file = os.path.splitext(user_arg)
+    # print(split_file)
+
+    file_name = split_file[0]  # Set filename to variable
+    file_ext = split_file[1]  # Set file extension to variable
+    # print(file_name)
+    # print(file_ext)
+
+    if file_ext == '.json':
+        file = open(user_arg, 'r')  # Open file
+        text = file.read()  # Read contents of file
+        file.close()
+        return text, file_name, file_ext
+    else:
+        print('########################################################################')
+        print('You did not select a proper .JSON file extension')
+        print(
+            'You will need to restart program and select the file return from Synchronoss')
+        print('The program will close in 5 seconds...')
+        time.sleep(5)
+        return False, False, False
+
+
+def format_to_JSON(text):
+    # Check to make sure the specific string (u'') exists in read file
+    for m in re.finditer('u\'\'', text):
+        # print('Character u\'\''' found at: {}'.format(str(m.span())))
+        if len(m.span()) > 0:
+            orgText = text.replace('u\'\'', '""')
+            # print(orgText)
+
+    # Check to make sure the specific string (u') exists in read file
+    for n in re.finditer('u\'', orgText):
+        # print('Character u\' found at: {}'.format(str(n.span())))
+        if len(n.span()) > 0:
+            orgText = orgText.replace('u\'', '"')
+            # print(orgText)
+
+    # Check to make sure the specific string (') exists in read file
+    for o in re.finditer('\'', orgText):
+        # print('Character \' found at: {}'.format(str(o.span())))
+        if len(o.span()) > 0:
+            orgText = orgText.replace('\'', '"')
+            # print(orgText)
+    return orgText
+
+
+def export_Converted_JSON(conv_text, file_loc):
+    # print(file_loc)
+    path = os.path.dirname(os.path.abspath(file_loc))
+    # print(path)
+    folder = 'Converted_Data'
+    join_path = os.path.join(path, folder)
+    # print(join_path)
+    if os.path.isdir(join_path):
+        pass
+    else:
+        os.mkdir(join_path)
+    save_fname = 'converted_data'
+    save_loc = os.path.join(join_path, save_fname+".json")
+    # print(save_loc)
+    if os.path.isfile(save_loc):
+        print('Can not create file, as folder Converted_Data already containes a converted_data.json file')
+        return False
+    else:
+        with open(save_loc, 'w') as outfile:
+            outfile.write(conv_text)
+        outfile.close()
+        return True
+
+
+# def write_converted_JSON():
+#     return True
+
+###########################
+# Begin Main Body Of Code #
+###########################
+
 passed_arg = get_args()
 # print('Passed arg: {}'.format(passed_arg))
 if str(passed_arg) != 'None':
@@ -94,67 +174,30 @@ else:
     print('You provided no arguemnts. Scanning current directory of this script for JSON files...')
     time.sleep(3)
 
+if checked_arg == 'File':
+    print('Time to load, read and process the File....')
+    orgText, fname, fext = get_JSON_File(passed_arg, location_info)
+    # print(orgText)
+    print('File successfully loaded. Time to remove unwanted \"u\"characters and change all \' to \"')
+    convertText = format_to_JSON(orgText)
+    # print(convertText)
+    print('Successfully changed characters and returned proper JSON format')
+    print('Preparting to export formatted data to \"Converted_data.json\"....')
+    status = export_Converted_JSON(convertText, location_info)
+    if status != False:
+        print('File created successfully!')
+    else:
+        print('There was an error while creating file. Please try again...')
+        time.sleep(3)
+        sys.exti()
+elif checked_arg == 'Directory':
+    print('Time to scan entire directory provided by user')
+else:
+    print('Scanning cwd of python file')
+
 # print(checked_arg)
 # print(location_info)
 # print(errors)
-
-# def get_JSON_File():  # Prompt the user to select the file needed, open the file and get filename
-#     root = tk.Tk()  # Initiate GUI file selector
-#     root.withdraw()  # Hide main GUI window
-#     path = askopenfilename(title="Select Synchronoss JSON File", filetypes=((
-#         "JSON File", "*.json"), ("All Files", "*.*")))  # Allow user to select JSON file
-
-#     full_file_name = os.path.basename(path)  # Get file path from selected file
-#     # Get just file name and extension
-#     split_file = os.path.splitext(full_file_name)
-#     # print(split_file)
-
-#     file_name = split_file[0]  # Set filename to variable
-#     file_ext = split_file[1]  # Set file extension to variable
-#     # print(file_name)
-#     # print(file_ext)
-
-#     if file_ext == '.json':
-#         file = open(path, 'r')  # Open file
-#         text = file.read()  # Read contents of file
-#         file.close()
-#         return text, file_name, file_ext
-#     else:
-#         print('########################################################################')
-#         print('You did not select a proper .JSON file extension')
-#         print(
-#             'You will need to restart program and select the file return from Synchronoss')
-#         print('The program will close in 5 seconds...')
-#         time.sleep(5)
-#         return False, False, False
-
-
-# def format_to_JSON(text):
-#     # Check to make sure the specific string (u'') exists in read file
-#     for m in re.finditer('u\'\'', text):
-#         # print('Character u\'\''' found at: {}'.format(str(m.span())))
-#         if len(m.span()) > 0:
-#             orgText = text.replace('u\'\'', '""')
-#             # print(orgText)
-
-#     # Check to make sure the specific string (u') exists in read file
-#     for n in re.finditer('u\'', orgText):
-#         # print('Character u\' found at: {}'.format(str(n.span())))
-#         if len(n.span()) > 0:
-#             orgText = orgText.replace('u\'', '"')
-#             # print(orgText)
-
-#     # Check to make sure the specific string (') exists in read file
-#     for o in re.finditer('\'', orgText):
-#         # print('Character \' found at: {}'.format(str(o.span())))
-#         if len(o.span()) > 0:
-#             orgText = orgText.replace('\'', '"')
-#             # print(orgText)
-#     return orgText
-
-
-# def write_converted_JSON():
-#     return True
 
 # # Examples to get current working directory of file
 # print("Path at terminal when executing this file")
@@ -174,13 +217,6 @@ else:
 # print("This file directory only")
 # print(os.path.dirname(full_path))
 
-# Get data from file selected by user, file name and file ext
-# orgText, fname, fext = get_JSON_File()
-# if orgText != False:
-#     newText = format_to_JSON(orgText)  # Format data and get proper JSON format
-# else:
-#     print('')
-
 # jsonText = json.loads(newText)
 # print(jsonText.keys())
 
@@ -190,6 +226,3 @@ else:
 
 # print(fname)
 # print(fext)
-
-# with open('converted_data.json', 'w') as outfile:
-#     outfile.write(orgText)
